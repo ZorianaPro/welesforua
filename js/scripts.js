19325c41f9a6726ab146b73c9e28531795d9e3d7
zoriana.pro <zoryanalesyk@gmail.com>
2022-04-18 14:47:30 +0200

    add

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,36 @@
 			}, 500);
 		}
 		hidePreloader();
+        setTimeout(lazyLoad, 1000);
 	});
 	
+    // This is "probably" IE9 compatible but will need some fallbacks for IE8
+    // - (event listeners, forEach loop)
+
+    // wait for the entire page to finish loading
+    function lazyLoad() {
+        var card_images = document.querySelectorAll('.card-image');
+
+        // loop over each card image
+        card_images.forEach(function (card_image) {
+            var image_url = card_image.getAttribute('data-image-full');
+            var content_image = card_image.querySelector('img');
+
+            // change the src of the content image to load the new high res photo
+            content_image.src = image_url;
+
+            // listen for load event when the new photo is finished loading
+            content_image.addEventListener('load', function () {
+                // swap out the visible background image with the new fully downloaded photo
+                card_image.style.backgroundImage = 'url(' + image_url + ')';
+                // add a class to remove the blur filter to smoothly transition the image change
+                card_image.className = card_image.className + ' is-loaded';
+            });
+
+        });
+
+    }
+
 	/* Navbar Scripts */
 	// jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
